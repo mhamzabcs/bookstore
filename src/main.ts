fe11a5ab8a8b5f4cffbab37a3ef07397f3651630
Muhammad Hamza <hamza2797@hotmail.com>
2022-12-27 22:31:40 +0500

    added unique user guard and validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidateInputPipe } from './core/pipes/validate.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidateInputPipe());
   await app.listen(4000);
 }
 bootstrap();
